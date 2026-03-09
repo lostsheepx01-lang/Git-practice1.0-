2e071e9ece4d9fd5d30242afad875797dfe685aa
Lost Sheep <lostsheepx01@gmail.com>
2026-03-09 20:59:03 +0530

    Implement temperature conversion between Celsius and Fahrenheit
    Fix formatting issues and correct Celsius output.

--- temp_converter.c
+++ temp_converter.c
@@ -1,1 +1,15 @@
-
+#include <stdio.h>
+int main() 
+{
+  float x ,y ;
+  float fahrenheit,celcius ;
+  printf("enter temperature in degree celcius");
+  scanf("%f",&x);
+  fahrenheit= (1.8* x)+32;
+  printf("given temperature in fahreneheit is %f \n",fahrenheit);
+  printf("enter temperature in degree fahrenheit");
+  scanf("%f",&y);
+  celcius= (y-32)*5/9;
+  printf("given temperature in degree celcius is %f \n" celcius);
+  return 0;
+}
